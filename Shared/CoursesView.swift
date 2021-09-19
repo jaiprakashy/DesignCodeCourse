9036f15f61640d9631d58ac547e01ac5cb0a193d
Jaiprakash Yadav <yadavjaiprakash20@gmail.com>
2021-09-19 14:23:33 +0530

    Created courses screen with navigation

---
+++ Shared/CoursesView.swift
@@ -0,0 +1,69 @@
+//
+//  CoursesView.swift
+//  DesignCodeCourse
+//
+//  Created by Jaiprakash Yadav on 19/09/21.
+//
+
+import SwiftUI
+
+struct CoursesView: View {
+    @State var show: Bool = false
+    @State var selectedItem: Course? = nil
+    @State var isDisabled = false
+    @Namespace var namespace
+    
+    var body: some View {
+        ZStack {
+            ScrollView {
+                VStack(spacing: 20) {
+                    ForEach(courses) { item in
+                        CourseItem(course: item)
+                            .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+                            .frame(width: 335, height: 250)
+                            .onTapGesture {
+                                withAnimation(.spring()) {
+                                    show.toggle()
+                                    selectedItem = item
+                                    isDisabled = true
+                                }
+                            }
+                            .disabled(isDisabled)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+            }
+            if let item = selectedItem {
+                ScrollView {
+                    CourseItem(course: item)
+                        .matchedGeometryEffect(id: item.id, in: namespace)
+                        .frame(height: 300)
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                show.toggle()
+                                selectedItem = nil
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                    isDisabled = false
+                                }
+                            }
+                        }
+                    VStack {
+                        ForEach(0 ..< 20) { item in
+                            CourseRow()
+                        }
+                    }
+                    .padding()
+                }
+                .background(Color("Background 1"))
+                .transition(.asymmetric(insertion: AnyTransition.opacity.animation(Animation.spring().delay(0.3)), removal: AnyTransition.opacity.animation(.spring())))
+                .edgesIgnoringSafeArea(.all)
+            }
+        }
+    }
+}
+
+struct CoursesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoursesView()
+    }
+}
---
+++ Shared/Items/CourseItem.swift
@@ -0,0 +1,41 @@
+//
+//  CourseItem.swift
+//  DesignCodeCourse
+//
+//  Created by Jaiprakash Yadav on 19/09/21.
+//
+
+import SwiftUI
+
+struct CourseItem: View {
+    var course: Course = courses[2]
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4.0) {
+            Spacer()
+            HStack {
+                Spacer()
+                Image(course.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+            }
+            Text(course.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+            Text(course.subtitle)
+                .font(.footnote)
+                .foregroundColor(Color.white)
+                .multilineTextAlignment(.center)
+        }
+        .padding(.all)
+        .background(course.color)
+        .clipShape(RoundedRectangle(cornerRadius: 22.0, style: .continuous))
+        .shadow(color: course.color.opacity(0.3), radius: 20, x: 0, y: 10)
+    }
+}
+
+struct CourseItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseItem()
+    }
+}
