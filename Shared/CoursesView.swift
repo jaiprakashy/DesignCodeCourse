e8d7a9a3f60f177d56f91772f25d3ebeb00af9b2
Jaiprakash Yadav <yadavjaiprakash20@gmail.com>
2021-09-19 20:16:20 +0530

    Finished Courses section

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -12,53 +12,169 @@ struct CoursesView: View {
     @State var selectedItem: Course? = nil
     @State var isDisabled = false
     @Namespace var namespace
+    @Namespace var namespace2
+    #if os(iOS)
+    @Environment (\.horizontalSizeClass) var horizontalSizeClass
+    #endif
     
     var body: some View {
         ZStack {
-            ScrollView {
-                VStack(spacing: 20) {
-                    ForEach(courses) { item in
-                        CourseItem(course: item)
-                            .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
-                            .frame(width: 335, height: 250)
-                            .onTapGesture {
-                                withAnimation(.spring()) {
-                                    show.toggle()
-                                    selectedItem = item
-                                    isDisabled = true
-                                }
-                            }
-                            .disabled(isDisabled)
-                    }
-                }
-                .frame(maxWidth: .infinity)
+            #if os(iOS)
+            if horizontalSizeClass == .compact {
+                tabBar
+            } else {
+                sidebar
             }
-            if let item = selectedItem {
-                ScrollView {
-                    CourseItem(course: item)
-                        .matchedGeometryEffect(id: item.id, in: namespace)
-                        .frame(height: 300)
-                        .onTapGesture {
-                            withAnimation(.spring()) {
-                                show.toggle()
-                                selectedItem = nil
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                    isDisabled = false
+            fullContent
+                .background(VisualEffectBlur(blurStyle: .systemMaterial).edgesIgnoringSafeArea(.all))
+            #else
+            content
+            fullContent
+                .background(VisualEffectBlur().edgesIgnoringSafeArea(.all))
+            #endif
+        }
+        .navigationTitle("Courses")
+    }
+    
+    var content: some View {
+        ScrollView {
+            VStack(spacing: 0) {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 160), spacing: 16)], spacing: 16) {
+                    ForEach(courses) { item in
+                        VStack {
+                            CourseItem(course: item)
+                                .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+                                .frame(height: 200)
+                                .onTapGesture {
+                                    withAnimation(.spring(response: 0.4, dampingFraction: 0.8, blendDuration: 0)) {
+                                        show.toggle()
+                                        selectedItem = item
+                                        isDisabled = true
+                                    }
                                 }
-                            }
+                                .disabled(isDisabled)
                         }
-                    VStack {
-                        ForEach(0 ..< 20) { item in
-                            CourseRow()
-                        }
+                        .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
                     }
-                    .padding()
                 }
-                .background(Color("Background 1"))
-                .transition(.asymmetric(insertion: AnyTransition.opacity.animation(Animation.spring().delay(0.3)), removal: AnyTransition.opacity.animation(.spring())))
-                .edgesIgnoringSafeArea(.all)
+                .padding(16)
+                .frame(maxWidth: .infinity)
+                
+                Text("Latest sections")
+                    .fontWeight(.semibold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 240))]) {
+                    ForEach(courseSections) { item in
+                        #if os(iOS)
+                        NavigationLink(destination: CourseDetail(namespace: namespace2)) {
+                            CourseRow(item: item)
+                        }
+                        #else
+                        CourseRow(item: item)
+                        #endif
+                    }
+                }
+                .padding()
             }
         }
+        .zIndex(1)
+        .navigationTitle("Courses")
+    }
+    
+    @ViewBuilder
+    var fullContent: some View {
+        if let courseItem = selectedItem {
+            ZStack(alignment: .topTrailing) {
+                CourseDetail(course: courseItem, namespace: namespace)
+                
+                CloseButton()
+                    .padding(16)
+                    .onTapGesture {
+                        withAnimation(.spring(response: 0.4, dampingFraction: 0.7, blendDuration: 0)) {
+                            show.toggle()
+                            selectedItem = nil
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                isDisabled = false
+                            }
+                        }
+                    }
+            }
+            .zIndex(2)
+            .frame(maxWidth: 712)
+            .frame(maxWidth: .infinity)
+        }
+    }
+    
+    var tabBar: some View {
+        TabView {
+            NavigationView {
+                content
+            }
+            .tabItem {
+                Image(systemName: "book.closed")
+                Text("Courses")
+            }
+            
+            NavigationView {
+                CourseList()
+            }
+            .tabItem {
+                Image(systemName: "list.bullet.rectangle")
+                Text("Tutorials")
+            }
+            
+            NavigationView {
+                CourseList()
+            }
+            .tabItem {
+                Image(systemName: "tv")
+                Text("Livestreams")
+            }
+            
+            NavigationView {
+                CourseList()
+            }
+            .tabItem {
+                Image(systemName: "mail.stack")
+                Text("Certificates")
+            }
+            
+            NavigationView {
+                CourseList()
+            }
+            .tabItem {
+                Image(systemName: "magnifyingglass")
+                Text("Search")
+            }
+        }
+    }
+    
+    @ViewBuilder
+    var sidebar: some View {
+        #if os(iOS)
+        NavigationView {
+            List {
+                NavigationLink(destination: CoursesView()) {
+                    Label("Courses", systemImage: "book.closed")
+                }
+                Label("Tutorials", systemImage: "list.bullet.rectangle")
+                Label("Livestreams", systemImage: "tv")
+                Label("Certificates", systemImage: "mail.stack")
+                Label("Search", systemImage: "magnifyingglass")
+            }
+            .listStyle(SidebarListStyle())
+            .navigationTitle("Learn")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "person.crop.circle")
+                }
+            }
+            
+            content
+        }
+        #endif
     }
 }
 
